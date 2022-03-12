034b20a4e2f77d12d6a4ae92b194e76c638b97a9
e-duu <ramangart2@gmail.com>
2022-03-12 13:33:21 +0700

    first commit

---
+++ lib/shared/theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kPrimaryColor = Color(0XFFFFEB00);
+Color kBlackColor = Color(0XFF212121);
+Color kRedColor = Color(0XFFFF0F0F);
+Color kWhiteColor = Color(0XFFFFFFFF);
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(
+  color: kPrimaryColor,
+);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: kBlackColor,
+);
+
+TextStyle redTextStyle = GoogleFonts.poppins(
+  color: kRedColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
---
+++ lib/ui/pages/splash_page.dart
@@ -0,0 +1,46 @@
+import 'package:adira_cats/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 240,
+              height: 42,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/adira_logo.png'),
+                ),
+              ),
+            ),
+            Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'CATS',
+                    style: blackTextStyle.copyWith(
+                      fontWeight: bold,
+                    ),
+                  ),
+                  Text(
+                    'Collection Activity and Tracking System',
+                    style: blackTextStyle,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
