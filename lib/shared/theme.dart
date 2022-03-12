1bc0617f69ee75060d7d765c5ad653057dc9aff4
muji101 <mujikuwat91@gmail.com>
2022-03-12 14:12:42 +0700

    login_page

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-double defaultMargin = 24.0;
-double defaultRadius = 17.0;
+double defaultMargin = 36.0;
+double defaultRadius = 8.0;
 
 Color kPrimaryColor = Color(0XFFFFEB00);
 Color kBlackColor = Color(0XFF212121);
