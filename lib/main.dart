2603023eb41b7db32fb047bf6f67e47719af1c08
e-duu <ramangart2@gmail.com>
2022-03-14 11:38:39 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:adira_cats/ui/pages/forgot_password_page.dart';
 import 'package:adira_cats/ui/pages/otp_page.dart';
 import 'package:adira_cats/ui/pages/login_page.dart';
+import 'package:adira_cats/ui/pages/sk_done_page.dart';
 import 'package:adira_cats/ui/pages/splash_page.dart';
-import 'package:adira_cats/ui/pages/success_page.dart';
+import 'package:adira_cats/ui/pages/vertification_done_page.dart';
 import 'package:adira_cats/ui/pages/trial_page.dart';
 import 'package:flutter/material.dart';
 
@@ -18,10 +19,11 @@ class MyApp extends StatelessWidget {
 
       /// NOTE: DONE
       // home: OtpPage(),
-      // home: SplashPage(),
-      // home: SuccessPage(),
+      home: SplashPage(),
+      // home: VertifivationDonePage(),
+      // home: SkDonePage(),
       // home: LoginPage(),
-      home: ForgotPasswordPage(),
+      // home: ForgotPasswordPage(),
 
       /// NOTE: ON PROGRESS
       // home: TrialPage(),
---
+++ lib/ui/pages/vertification_done_page.dart
@@ -0,0 +1,84 @@
+import 'package:adira_cats/shared/theme.dart';
+import 'package:adira_cats/ui/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+import 'package:styled_text/styled_text.dart';
+
+class VertifivationDonePage extends StatelessWidget {
+  const VertifivationDonePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kWhiteColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 140,
+                    height: 140,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                          'assets/icon_verification_done.png',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Text(
+                          'Berhasil!',
+                          style: blackTextStyle.copyWith(
+                            fontWeight: bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 24,
+                        ),
+                        StyledText(
+                          text:
+                              'Akun <bold>Edward Einselton</bold> sudah terdaftar!\nSilakan Login kembali.',
+                          tags: {
+                            'bold': StyledTextTag(
+                              style: TextStyle(fontWeight: bold),
+                            ),
+                          },
+                          style: blackTextStyle.copyWith(
+                            fontWeight: light,
+                            fontSize: 13,
+                          ),
+                          textAlign: TextAlign.center,
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              bottom: 36,
+            ),
+            child: CustomButton(
+              title: 'Ok',
+              onPressed: () {},
+              margin: EdgeInsets.symmetric(
+                horizontal: 36,
+              ),
+              color: kPrimaryColor,
+              fontWeight: bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
