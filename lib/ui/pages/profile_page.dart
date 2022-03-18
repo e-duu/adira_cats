5c1f0961012e9adff33b23704d7b46ab2adee2db
Hudzaifah <hudz1357@gmail.com>
2022-03-18 08:26:45 +0700

    kepo commit

--- lib/ui/pages/profile_page.dart
+++ lib/ui/pages/profile_page.dart
@@ -41,6 +41,7 @@ class ProfilePage extends StatelessWidget {
               ),
               shape: BoxShape.circle,
               image: DecorationImage(
+                fit: BoxFit.cover,
                 image: AssetImage(
                   "assets/image_user.png",
                 ),
@@ -52,152 +53,14 @@ class ProfilePage extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    MouseRegion(
-                      cursor: SystemMouseCursors.click,
-                      child: GestureDetector(
-                        onTap: () => showDialog<String>(
-                          context: context,
-                          builder: (BuildContext context) => Container(
-                            height: 200,
-                            width: double.infinity,
-                            child: AlertDialog(
-                              titlePadding: EdgeInsets.only(
-                                top: 10.h,
-                              ),
-                              contentPadding: EdgeInsets.symmetric(
-                                vertical: 20.h
-                              ),
-                              title: Container(
-                                margin: EdgeInsets.only(
-                                  top: 48.h,
-                                ),
-                                child: Text(
-                                  'Ganti Photo',
-                                  style: blackTextStyle.copyWith(
-                                    fontWeight: bold,
-                                    fontSize: 18.sp,
-                                  ),
-                                  textAlign: TextAlign.center,
-                                ),
-                              ),
-                              content: Container(
-                                width: double.infinity,
-                                margin: EdgeInsets.symmetric(
-                                  horizontal: 36
-                                ),
-                                child: Column(
-                                  children: [
-                                    Text(
-                                      "Upload Photo",
-                                      style: darkGreyTextStyle.copyWith(
-                                        fontSize: 13,
-                                        fontWeight: semibold
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 12,
-                                    ),
-                                    CustomButton(
-                                      title: "Upload", 
-                                      textStyle: whiteTextStyle,
-                                      color: kPrimaryColor,
-                                      onPressed: () => showDialog(
-                                        context: context,
-                                        builder: (BuildContext context) {
-                                          return AlertDialog(
-                                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-                                            title: Text('Silahkan Pilih Media'),
-                                            content: Container(
-                                              height: MediaQuery.of(context).size.height / 10,
-                                              child: Column(
-                                                children: <Widget>[
-                                                  TextButton(
-                                                    onPressed: () {
-                                                      Navigator.pop(context);
-                                                    },
-                                                    child: Row(
-                                                      children: <Widget>[
-                                                        Icon(Icons.image),
-                                                        Text('From Gallery'),
-                                                      ],
-                                                    ),
-                                                  ),
-                                                  SizedBox(
-                                                    height: 6,
-                                                  ),
-                                                  TextButton(
-                                                    onPressed: () {
-                                                      Navigator.pop(context);
-                                                    },
-                                                    child: Row(
-                                                      children: <Widget>[
-                                                        Icon(Icons.camera),
-                                                        Text('From Camera'),
-                                                      ],
-                                                    ),
-                                                  ),
-                                                ],
-                                              ),
-                                            ),
-                                          );
-                                        }
-                                      ),
-                                    ),
-                                  ],
-                                )
-                              ),
-                              actions: <Widget>[
-                                Container(
-                                  width: 380.w,
-                                  margin: EdgeInsets.only(
-                                    right: 36.w,
-                                    left: 36.w,
-                                    bottom: 48.h,
-                                  ),
-                                  child: Column(
-                                    children: [
-                                      CustomButtonBorder(
-                                        title: "Batalkan",
-                                        titleColor: kDarkGreyColor,
-                                        onPressed: () => Navigator.pop(context),
-                                        borderColor: kDarkGreyColor,
-                                        borderWidth: 2,
-                                        fontWeight: light,
-                                      ),
-                                      SizedBox(
-                                        height: 12.h,
-                                      ),
-                                      CustomButton(
-                                        title: 'Tetap Edit',
-                                        textStyle: whiteTextStyle,
-                                        color: kPrimaryColor,
-                                        onPressed: () {},
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                              actionsPadding: EdgeInsets.only(
-                                bottom: 10.h,
-                              ),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18.r),
-                              ),
-                            ),
-                          ),
-                        ),
-                        child: Container(
-                          width: 42,
-                          height: 42,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            color: kPrimaryColor,
-                          ),
-                          child: Icon(
-                            Icons.edit_outlined
-                          ),
-                        ),
+                    Container(
+                      width: 42,
+                      height: 42,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: kPrimaryColor,
                       ),
+                      child: Icon(Icons.edit_outlined),
                     ),
                   ],
                 )
@@ -465,7 +328,8 @@ class ProfilePage extends StatelessWidget {
             CustomTextField(
               readOnly: true,
               hintColor: kBlackColor,
-              hintText: "Jln. Raya Bau Petrikor no.8, Kec. Camat, Kab. Kabup, Provinsi DKI Jakarta",
+              hintText:
+                  "Jln. Raya Bau Petrikor no.8, Kec. Camat, Kab. Kabup, Provinsi DKI Jakarta",
               maxLines: 2,
               margin: EdgeInsets.only(
                 top: 12.h,
