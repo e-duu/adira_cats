8c2fbc8101ae8dde98bf3cade7a1c782a5b10dc9
Hudzaifah <hudz1357@gmail.com>
2022-03-23 20:03:51 +0700

    kepo commit

--- lib/main.dart
+++ lib/main.dart
@@ -108,7 +108,7 @@ class MyApp extends StatelessWidget {
           // home: ForgotPasswordPage(),
           // home: ResetPasswordPage(),
           // home: ProfilePage(),
-          home: RegisterPage(),
+          // home: RegisterPage(),
           // home: RegisterSecondPage(),
           // home: RegisterThirdPage(),
           // home: RegisterThirdDonePage(),
