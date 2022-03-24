9dad270f441cb501c9240685ff510b51fbd084a1
Hudzaifah <hudz1357@gmail.com>
2022-03-24 21:28:07 +0700

    kepo commit

--- lib/ui/widgets/custom_bar_chart.dart
+++ lib/ui/widgets/custom_bar_chart.dart
@@ -3,50 +3,16 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 
 class CustomBarChart extends StatelessWidget {
-  final String itemParent1;
-  final String itemParent2;
-  final String itemParent3;
-  final String itemParent4;
-  final String itemParent5;
-  final String itemParent6;
-  final String itemParent7;
-  final int itemValue1;
-  final int itemValue2;
-  final int itemValue3;
-  final int itemValue4;
-  final int itemValue5;
-  final int itemValue6;
-  final int itemValue7;
+  final List<Sales> listGrafik;
 
   const CustomBarChart({
     Key? key,
-    required this.itemParent1,
-    required this.itemParent2,
-    required this.itemParent3,
-    required this.itemParent4,
-    required this.itemParent5,
-    required this.itemParent6,
-    required this.itemParent7,
-    required this.itemValue1,
-    required this.itemValue2,
-    required this.itemValue3,
-    required this.itemValue4,
-    required this.itemValue5,
-    required this.itemValue6,
-    required this.itemValue7,
+    required this.listGrafik,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    var data = [
-      Sales(itemParent1, itemValue1),
-      Sales(itemParent2, itemValue2),
-      Sales(itemParent3, itemValue3),
-      Sales(itemParent4, itemValue4),
-      Sales(itemParent5, itemValue5),
-      Sales(itemParent6, itemValue6),
-      Sales(itemParent7, itemValue7),
-    ];
+    var data = listGrafik;
 
     var series = [
       charts.Series(
