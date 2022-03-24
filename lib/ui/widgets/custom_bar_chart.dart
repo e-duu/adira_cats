906a1ac417cd859b772cf3faf29aeba5f5da0cb2
Hudzaifah <hudz1357@gmail.com>
2022-03-24 10:35:19 +0700

    kepo commit

--- lib/ui/widgets/custom_bar_chart.dart
+++ lib/ui/widgets/custom_bar_chart.dart
@@ -3,35 +3,49 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 
 class CustomBarChart extends StatelessWidget {
-  final int mon;
-  final int tue;
-  final int wed;
-  final int thu;
-  final int fri;
-  final int sat;
-  final int sun;
+  final String itemParent1;
+  final String itemParent2;
+  final String itemParent3;
+  final String itemParent4;
+  final String itemParent5;
+  final String itemParent6;
+  final String itemParent7;
+  final int itemValue1;
+  final int itemValue2;
+  final int itemValue3;
+  final int itemValue4;
+  final int itemValue5;
+  final int itemValue6;
+  final int itemValue7;
 
   const CustomBarChart({
     Key? key,
-    required this.mon,
-    required this.tue,
-    required this.wed,
-    required this.thu,
-    required this.fri,
-    required this.sat,
-    required this.sun,
+    required this.itemParent1,
+    required this.itemParent2,
+    required this.itemParent3,
+    required this.itemParent4,
+    required this.itemParent5,
+    required this.itemParent6,
+    required this.itemParent7,
+    required this.itemValue1,
+    required this.itemValue2,
+    required this.itemValue3,
+    required this.itemValue4,
+    required this.itemValue5,
+    required this.itemValue6,
+    required this.itemValue7,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var data = [
-      Sales("Sen", mon),
-      Sales("Sel", tue),
-      Sales("Rab", wed),
-      Sales("Kam", thu),
-      Sales("Jum", fri),
-      Sales("Sab", sat),
-      Sales("Min", sun),
+      Sales(itemParent1, itemValue1),
+      Sales(itemParent2, itemValue2),
+      Sales(itemParent3, itemValue3),
+      Sales(itemParent4, itemValue4),
+      Sales(itemParent5, itemValue5),
+      Sales(itemParent6, itemValue6),
+      Sales(itemParent7, itemValue7),
     ];
 
     var series = [
@@ -49,7 +63,7 @@ class CustomBarChart extends StatelessWidget {
       series,
       barRendererDecorator: charts.BarLabelDecorator<String>(),
       animate: true,
-      // domainAxis: charts.OrdinalAxisSpec(renderSpec: charts.NoneRenderSpec()), // Untuk Menghilangkan info hari
+      // domainAxis: charts.OrdinalAxisSpec(renderSpec: charts.NoneRenderSpec()), // Untuk Menghilangkan Parentnya
     );
 
     return Container(
