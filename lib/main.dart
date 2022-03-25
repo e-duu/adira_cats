4dbf7a4513a62eb8ea2d1da38eda27da0c41c9f1
muji101 <mujikuwat91@gmail.com>
2022-03-25 20:27:56 +0700

    page

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,12 @@ import 'package:adira_cats/ui/pages/create_st_contract_null_page.dart';
 import 'package:adira_cats/ui/pages/create_st_cotract_number_page.dart';
 import 'package:adira_cats/ui/pages/create_st_page.dart';
 import 'package:adira_cats/ui/pages/error_page.dart';
-import 'package:adira_cats/ui/pages/handover_st_detail_page.dart';
-import 'package:adira_cats/ui/pages/handover_st_confirmed_page.dart';
-import 'package:adira_cats/ui/pages/handover_st_page.dart';
-import 'package:adira_cats/ui/pages/handover_st_search_nil_page.dart';
-import 'package:adira_cats/ui/pages/handover_st_search_page.dart';
+// import 'package:adira_cats/ui/pages/handover_sk_detail_page.dart';
+// import 'package:adira_cats/ui/pages/handover_sk_confirmed_page.dart';
+// import 'package:adira_cats/ui/pages/handover_sk_page.dart';
+// import 'package:adira_cats/ui/pages/handover_sk_search_nil_page.dart';
+// import 'package:adira_cats/ui/pages/handover_sk_search_page.dart';
 import 'package:adira_cats/ui/pages/home_page.dart';
-import 'package:adira_cats/ui/pages/loading_page.dart';
 import 'package:adira_cats/ui/pages/login_page.dart';
 import 'package:adira_cats/ui/pages/main_page.dart';
 import 'package:adira_cats/ui/pages/message_page.dart';
@@ -22,7 +21,6 @@ import 'package:adira_cats/ui/pages/register_fourth_page.dart';
 import 'package:adira_cats/ui/pages/register_page.dart';
 import 'package:adira_cats/ui/pages/register_second_page.dart';
 import 'package:adira_cats/ui/pages/profile_page.dart';
-import 'package:adira_cats/ui/pages/register_third_done_page.dart';
 import 'package:adira_cats/ui/pages/register_third_page.dart';
 import 'package:adira_cats/ui/pages/report_detail_mv_page%20.dart';
 import 'package:adira_cats/ui/pages/report_detail_mv_result_page.dart';
@@ -44,17 +42,9 @@ import 'package:adira_cats/ui/pages/report_detail_visit_page.dart';
 import 'package:adira_cats/ui/pages/report_detail_visit_result_detail_page.dart';
 import 'package:adira_cats/ui/pages/report_detail_visit_result_page.dart';
 import 'package:adira_cats/ui/pages/report_page.dart';
-import 'package:adira_cats/ui/pages/request_unit_page.dart';
 import 'package:adira_cats/ui/pages/reset_password_page.dart';
-import 'package:adira_cats/ui/pages/share_unit_found_page.dart';
-import 'package:adira_cats/ui/pages/share_unit_page.dart';
 import 'package:adira_cats/ui/pages/st_done_page.dart';
 import 'package:adira_cats/ui/pages/splash_page.dart';
-import 'package:adira_cats/ui/pages/unit_search_detail_page.dart';
-import 'package:adira_cats/ui/pages/unit_search_found_page.dart';
-import 'package:adira_cats/ui/pages/unit_search_nil_page.dart';
-import 'package:adira_cats/ui/pages/unit_search_page.dart';
-import 'package:adira_cats/ui/pages/unit_search_pull_page.dart';
 import 'package:adira_cats/ui/pages/verification_done_page.dart';
 import 'package:adira_cats/ui/pages/testing.dart';
 import 'package:flutter/material.dart';
@@ -79,7 +69,6 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           // routes: {
           //   '/': (context) => SplashPage(),
-          //   '/load': (context) => LoadPage(),
           //   '/login': (context) => LoginPage(),
           //   '/main': (context) => MainPage(),
           // },
@@ -96,34 +85,35 @@ class MyApp extends StatelessWidget {
           // home: RegisterPage(),
           // home: RegisterSecondPage(),
           // home: RegisterThirdPage(),
-          home: RegisterThirdDonePage(),
           // home: RegisterFourthPage(),
           // home: CreateStPage(),
           // home: StDonePage(),
-          // home: HandoverStPage(),
+          // home: HandoverSkPage(),
           // home: ReportDetailPage(),
-          // home: HandoverStSearchPage(),
-          // home: HandoverStDetailPage(),
-          // home: HandoverStSearchNilPage(),
-          // home: HandoverStConfirmedPage(),
+          // home: HandoverSkSearchPage(),
+          // home: HandoverSkDetailPage(),
+          // home: HandoverSkSearchNilPage(),
+          // home: HandoverSkConfirmedPage(),
           // home: CreateStContractNumberPage(),
           // home: CreateStContractNullPage(),
           // home: ReportDetailPage(),
           // home: ReportDetailProdexPage(),
           // home: ReportPage(),
-          // home: ReportPage(),
           // home: ReportDetailPage(),
           // home: ReportDetailProdexPage(),
           // home: ReportDetailProdexResultPage(),
           // home: ReportDetailVisitPage(),
-          // home: UnitSearchPage(),
-          // home: UnitSearchFoundPage(),
-          // home: UnitSearchNilPage(),
           // home: ReportDetailPullFeePage(),
           // home: MessageRoomPage(),
+          // home: ReportDetailProdexNihilPage(),
+          // home: ReportDetailProdexResultDetailPage(),
+          // home: ReportDetailProdexResultPage(),
           // home: ReportDetailVisitPage(),
+          // home: ReportDetailVisitNihilPage(),
           // home: ReportDetailVisitResultDetailPage(),
+          // home: ReportDetailVisitResultPage(),
           // home: ReportDetailPullFeePage(),
+          // home: ReportDetailPullFeeNihilPage(),
           // home: ReportDetailPullFeeResultDetailPage(),
           // home: ReportDetailPullFeeResultPage(),
           // home: ReportDetailStPage(),
@@ -134,9 +124,7 @@ class MyApp extends StatelessWidget {
           /// NOTE: ON PROGRESS
           // home: TestingPage(),
           // home: ReportDetailMvPage(),
-          // home: ReportDetailMvResultPage(),
-          // home: UnitSearchDetailPage(),
-          // home: UnitSearchPullPage(),
+          home: ReportDetailMvResultPage(),
         ),
       ),
     );
