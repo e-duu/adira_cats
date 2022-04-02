39678a0b653ccd0551827ac013906054dd08a1ad
RamaDias123 <Nurdias45.com>
2022-04-02 10:13:17 +0700

    rama

--- lib/main.dart
+++ lib/main.dart
@@ -140,7 +140,7 @@ class MyApp extends StatelessWidget {
           // home: LocalNotification(),
           // home: NotificationPage(),
           // home: ProfilePage(),
-          // home: HomePage(),
+          home: HomePage(),
           // home: HomePageOption(),
           // home: ErrorPage(),
           // home: LoadPage(),
@@ -161,7 +161,7 @@ class MyApp extends StatelessWidget {
           // home: HandoverStConfirmedPage(),
 
           /// NOTE : REPORT PAGE
-          home: ReportPage(),
+          // home: ReportPage(),
           // home: ReportDetailPage(),
 
           /// NOTE : VISIT PAGE
