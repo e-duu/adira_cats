7f4b5f1b00b2a51429b37c402eceb0dafbdb3420
e-duu <ramangart2@gmail.com>
2022-04-05 13:32:47 +0700

    commit

--- lib/ui/pages/testing.dart
+++ lib/ui/pages/testing.dart
@@ -29,17 +29,9 @@ class TestingPage extends StatelessWidget {
           child: Column(
             children: [
               // CustomCheckbox(),
-<<<<<<< HEAD
-              CustomInputSearchDropdown(
-                onPressedSuffix: () {},
-                hintText: 'Cari Unit menggunakan Nomor Kontrak ...',
-              ),
-              CustomInputTextFieldTags(),
-=======
               // CustomInputSearchNoContract(onPressedSuffix: (){}, hintText: 'Cari Unit menggunakan Nomor Kontrak ...'),
               // CustomInputSearchPlatNo(),
-              CustomInputSearchMulti()
->>>>>>> 5bedcdc779047da1b924ee9fa23068b5e29c9700
+              // CustomInputSearchMulti(),
               // CustomLineChart(
               //   listGrafik: [
               //     LineChart(1, 100),
